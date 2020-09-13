672c4c98baab450ed5e36c256bc52078a44ab244
dc_langx <1102301590@qq.com>
2020-09-13 23:59:04 +0800

    button 组件

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,12 @@
 import React, { useState } from 'react'
-import logo from './logo.svg'
-import './App.css'
-import LikeButton from './components/LikeButton'
-import MousesTracker from './components/MousesTracker'
-import useMousePosition from './hooks/useMousePosition'
+import Button, { ButtonType, ButtonSize } from './components/Button/button'
 function App() {
-  const [show, setShow] = useState(true)
-  const positions = useMousePosition()
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <button
-          onClick={() => {
-            setShow(!show)
-          }}
-        >
-          toggleShowMouse
-        </button>
-        <p>
-          X:{positions.x},Y:{positions.y}
-        </p>
-        {/* {show && <MousesTracker />} */}
-        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-          Learn React
-        </a>
+        <Button btnType={ButtonType.Primary} size={ButtonSize.Large}>Hello</Button>
+        <Button btnType={ButtonType.Link} href='https://www.baidu.com'>Baidu</Button>
+        <hr />
       </header>
     </div>
   )
